1a6e245943e290c94a0d8919d07bb8d27775acfa
Audun <audunhilden@live.no>
2019-12-05 22:18:32 +0100

    10$ bet on pUBLISHED

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,51 +2,61 @@
 
 {{-- https://laravel.com/docs/5.4/blade#stacks --}}
 @push('head')
+  @include('_partials.header')
 
-    @section('title', ' Audun Hilden')
+
+    @section('title', ' Discord Templates')
 
     {{-- https://laravel.com/docs/5.4/blade#template-inheritance --}}
     @section('body')
 
-    <!-- @include('_partials.header-project') -->
-
-
     <div class="center-div text-white">
       <div class="row wrapper no-valign-wrap-on-mobile">
-        <div class="col s12 l19 desktop">
-          <h1 class="project_title text-center" style="margin-top:8.5rem;">
-           title
-        </h1>
-        <div class="project_text text-center mt-6">
-          - desc
-        </div>
-        <p>
-          <br>
-          <div class="wrapperwork">
-            <div class="featured-image center-oldclass">
-              <div class="row justify-content-center">
-                <div class="col-sm-12 col-md-10 col-lg-8">
-                  <div class="browser">
-                    <div class="header">
-                      <ul class="dots">
-                        <li>&nbsp;</li><li>&nbsp;</li><li>&nbsp;</li>
-                      </ul>
-                    </div>
-                    <img class="img-fluid" src="img/froendesktop.png" alt="Website" style="border:1px solid #eaedf2;">
-                  </div>
-
-                  <h1 class="project_title text-center" style="margin-top:12.5rem;">
-                    Services
-                  </h1>
-                  <div class="project_text text-center mt-6">
-                    Web-Design, Mobile Design, Email services, CMS, Responsive
-                  </div>
-                </div>
-              </div>
+        <div class="col s12 l19">
+          <div class="leftspan column">
+            <h4>Hi, I'm</h4>
+            <h1 style="margin: 15px 0;">
+              Audun Hilden
+            </h1>
+            <div class="text">
+              <span style="color:#4776ae;">
+                Front-End Developer
+              </span>, UI Designer and
+              caffeine addicted. I'll make your dreams come online,
+              just keep scrolling.
             </div>
           </div>
+          <div class="rightspan column">
+            <img src="https://i.imgur.com/5h83FYr.png" style="border-radius: 500px;width:200px;">
         </div>
       </div>
     </div>
+  </div>
+</div>
+<div style="margin-top:300px;">
+  &nbsp;
+  @foreach ($page->featured as $featured)
+  </div>
+  <div class="body" style="background:{{$featured->maincolor}};">
+    <div class="wrapper">
+      <div class="leftspan column">
+        <h2 class="title">{{$featured->title}}</h2>
+        <p>
+          <br>
+        {{$featured->personal}}
+      </div>
+      <div class="rightspan column">
+        <a href="/project/{{$featured->title}}" class="btn-primary-self btn-white" style="color:{{$featured->maincolor}}">
+          See case study
+        </a>
+      </div>
+
+      <img src="{{$featured->desktop}}" style="padding-top:50px;">
+
+    </div>
+    <div class="clearfix">&nbsp;</div>
+  </div>
+
+  @endforeach
 
 @endsection
