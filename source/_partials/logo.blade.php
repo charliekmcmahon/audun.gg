d20fbfccd6b6c8950bda9957ac56e310df2512bf
Audun Hilden <audunhilden@live.no>
2020-07-19 06:25:33 +0200

    yes

--- source/_partials/logo.blade.php
+++
@@ -1,5 +0,0 @@
-<li class="p-2">
-    <a href="{{ $href }}" class="no-underline" target="_blank">
-        <img src="{{ $src }}" alt="{{ $alt }}" width="{{ $width }}" class="block">
-    </a>
-</li>
