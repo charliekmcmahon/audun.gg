d2dd231059fd9604a627a4a198c6b17de47dbde0
Audun Hilden <audunhilden@live.no>
2020-09-11 16:27:37 +0200

    helg :D

--- work/hapro/script.js
+++ work/hapro/script.js
@@ -1,21 +1,34 @@
 function closeError() {
   let div = document.getElementById('errorMsg');
-  div.style.opacity = 0;
+  div.style.display = 'hidden';
 }
 
-try {
-  function showError() {
-    let x = document.getElementById('errorMsg');
-    x.style.opacity = 1;
-    setInterval(() => {
-      if ((x.opacity = '0')) {
-        x.style.opacity = '0';
-        console.log('[INFO] Error melding forsvant');
-      } else {
-        x.style.opacity = '1';
-      }
-    }, 10000);
-  }
-} catch (error) {
-  console.log(error.message);
+function showError() {
+  let x = document.getElementById('errorMsg');
+  x.style.display = 'block';
+  setInterval(function () {
+    if ((x.style.display = 'hidden')) {
+      x.style.display = 'hidden';
+      console.log('[INFO] Error melding forsvant');
+    } else {
+      x.style.display = 'hidden';
+    }
+  }, 500);
+}
+
+const admin = document.getElementById('isOpenAdmin');
+const tilbud = document.getElementById('isOpenTilbud');
+
+function openTilbud() {
+  tilbud.classList.toggle('hidden');
+  admin.classList.add('hidden');
+}
+function openAdmin() {
+  admin.classList.toggle('hidden');
+  tilbud.classList.add('hidden');
+}
+
+function closeAllMenus() {
+  admin.classList.add('hidden');
+  tilbud.classList.add('hidden');
 }
